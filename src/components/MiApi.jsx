9f163f921a1380fc0bae970959ff99deecbb96bd
Juan Jo Contreras <juanjocontrerascad@gmail.com>
2022-09-10 15:49:26 -0300

    componentes version3

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 function MiApi() {
   // 3.
   // Estado de los Datos.  
-  // Callback se llama en el montaje
-    const [datos, setDatos] = useState([]); // Array Vacio // Callback se llama en el montaje / datos guardará los valores traídos desde la API.
+                                                                                        // Callback se llama en el montaje
+    const [datos, setDatos] = useState([]);                                             // Array Vacio // Callback se llama en el montaje / datos guardará los valores traídos desde la API.
 
   // Estado de la Busqueda. 
     const [busqueda, setBusqueda] = useState('');    
@@ -16,29 +16,29 @@ function MiApi() {
     }, []);
   
     // 7. Captura en el Dom del componente item.
-    const Datos = ({item}) => <p className="card">{item.content} {item.title}</p> // Captura de map (item) imprime(item.content) //Por cada item que recorra el map imprime un parrafo con el contenido del item. // Imprime el item.content
+    const Datos = ({item}) => <p className="card">{item.content} {item.title}</p>         // Captura de map (item) imprime(item.content) //Por cada item que recorra el map imprime un parrafo con el contenido del item. // Imprime el item.content
     
     // 1. Función que consulta la API
     const consultarInformacion = async () => {
       const url = 'https://www.feriadosapp.com/api/laws.json';      
       const res = await fetch(url);
-      const {data} = await res.json(); // Formatea a json
-          // Lo que contiene el endpoint esta dentro de data
+      const {data} = await res.json();                                                      // Formatea a json
+                                                                                            // Lo que contiene el endpoint esta dentro de data
       const feriados = data.map((f) => {
-        // Dentro de feriados mapee data y traiga el id y el contenido
+                                                                                             // Dentro de feriados mapee data y traiga el id y el contenido
         // Retorne un objeto
         // Retorno los datos de id y content
         return {   
-          id: f.id, content: f.content, title: f.title,   link: f.link,
+          id: f.id, content: f.content, title: f.title,
         };
       });
     
           //6.Actualizamos el estado      
       setDatos(feriados);    
-       //   Actualizamos el estado (Hook)   // Solicito a setDatos que capture todo lo que esta dentro de la API y lo guarde en datos
+       //   Actualizamos el estado (Hook)                                                   // Solicito a setDatos que capture todo lo que esta dentro de la API y lo guarde en datos
            
     }
-    //    4. Input de busqueda de Leyes de los feriados
+    //  4. Input de busqueda de Leyes de los feriados
     return (
       <div className="d-flex flex-column">
         <div className="d-flex">              
@@ -58,15 +58,15 @@ function MiApi() {
         datos.filter((e) => {          
             if(busqueda === ' '){                           //Si el input esta vacio retorna e= datos de endpoint (API) capturados en dato.
               return e;
-            } else if (                                   //Si no esta vacio, imprime el contenido
+            } else if (                                     //Si no esta vacio, imprime el contenido
               e.content
-              .toLocaleLowerCase()                      // En minuscula
-              .includes(busqueda.toLocaleLowerCase()) //Que incluya el contenido del input busqueda
+              .toLocaleLowerCase()                          // En minuscula
+              .includes(busqueda.toLocaleLowerCase())       //Que incluya el contenido del input busqueda
               )  {
               return e;
-              }                                    // Cuando realice conexion, lo mapee
-            }).map((item) => (                    // Al resultado del filter mapealo e imprimelo en el Dom. //Al conectar puntomap c/filter somete al punto map // Renderizado.
-            <Datos key={item.id} item={item} />  //Componente de impresion.
+              }                                             // Cuando realice conexion, lo mapee
+            }).map((item) => (                              // Al resultado del filter mapealo e imprimelo en el Dom. //Al conectar puntomap c/filter somete al punto map // Renderizado.
+            <Datos key={item.id} item={item} />              //Componente de impresion.
           ))}
       </div>    
       </div>
